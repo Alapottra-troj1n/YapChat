c5d2c201eb46aa54124beffe90029bea3347f6c8
Alapottra <alapottratrojon@gmail.com>
2022-10-06 11:09:47 +0600

    userChats functionality done and visibe on chats

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,23 +1,58 @@
-import React from 'react';
+import { doc, onSnapshot } from 'firebase/firestore';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import { db } from '../firebase.init';
 
 const Chats = () => {
+
+    const {currentUser} = useContext(AuthContext);
+
+    const [chats, setChats] = useState([]);
+
+    useEffect(() => {
+
+        const getChats = () => {
+            const unsub = onSnapshot(doc(db, 'userChats', currentUser.uid), (doc) => {
+                setChats(doc.data());
+            });
+        
+
+        return () => {
+            unsub();
+        }
+    }
+
+        currentUser.uid && getChats();
+
+    }, [currentUser.uid])
+
+    console.log(chats)
+
+
     return (
         <div className="p-5 flex flex-col lg:gap-5">
 
-            <div className="flex items-center lg:gap-5 gap-2 p-3 rounded-md flex-col lg:flex-row" >
-                <div style={{ backgroundImage: 'url(profile.jpg)', backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }} className="lg:h-16 lg:w-16 h-8 w-8  rounded-full" >
+          {
+            Object.entries(chats)?.map(chat => (
+
+                <div key={chat[0]} className="flex items-center lg:gap-5 gap-2 p-3 rounded-md flex-col lg:flex-row" >
+                <div style={{ backgroundImage: `url(${chat[1].userInfo.photoURL})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }} className="lg:h-16 lg:w-16 h-8 w-8  rounded-full" >
                 </div>
                 <div className="text-white">
-                        <h2 className="lg:text-xl text-md font-semibold" >Alapottra</h2>
-                        <div className="hidden md:block">
-                            <h2>okey dude I will try</h2>
-                        </div>
+                    <h2 className="lg:text-xl text-md font-semibold" >{chat[1].userInfo.displayName}</h2>
+                    <div className="hidden md:block">
+                        <h2>okey dude I will try</h2>
+                    </div>
                 </div>
             </div>
 
-            
 
-            
+            ))
+          }
+
+
+
+
         </div>
     );
 };
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -19,13 +19,10 @@ const Search = () => {
      
         try {
             const querySnapshot = await getDocs(q);
-            if(querySnapshot.empty) {
-                setErr(true)
-                return;
-            }
+        
             querySnapshot.forEach((doc) => {
                 setUser(doc.data());
-                console.log(doc.data());
+            
             });
             setErr(false);
         } catch (error) {
@@ -73,16 +70,17 @@ const Search = () => {
                     },
                     [combinedId + '.date'] : serverTimestamp()
                 })
+
             }
 
 
 
         } catch (error) {
-            console.log(error)
-            setUser(null);
-            setUsername("")
+         console.log(error)
         }
-
+        
+        setUser(null);
+        setUsername("")
 
 
     }
@@ -90,18 +88,20 @@ const Search = () => {
     return (
         <>
             <div>
-                <input type="text" name="search" onKeyDown={handleKey} onChange={(e) => setUsername(e.target.value)} className=" p-2 pl-5 w-full bg-slate-500 cursor-pointer " placeholder="Find a user" />
+                <input type="text" name="search" onKeyDown={handleKey} value={username} onChange={(e) => setUsername(e.target.value)} className=" p-2 pl-5 w-full bg-slate-500 cursor-pointer " placeholder="Find a user" />
             </div>
             {err && <h2 className="text-red-500 text-center font-semibold mt-2" >User not found</h2>}
 
             {
-                user && <div onClick={handleSelect} className="flex items-center lg:gap-5 gap-2 p-3 rounded-md flex-col lg:flex-row bg-slate-500 hover:bg-slate-700  cursor-pointer transition-all shadow-md" >
+                user && (
+                    <div onClick={handleSelect} className="flex items-center lg:gap-5 gap-2 p-3 rounded-md flex-col lg:flex-row bg-slate-500 hover:bg-slate-700  cursor-pointer transition-all shadow-md" >
                     <div style={{ backgroundImage: `url(${user.photoURL})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }} className="lg:h-16 lg:w-16 h-8 w-8  rounded-full" >
                     </div>
                     <div className="text-white">
                         <h2 className="lg:text-xl text-md font-semibold" >{user.displayName}</h2>
                     </div>
                 </div>
+                )
 
             }
         </>
